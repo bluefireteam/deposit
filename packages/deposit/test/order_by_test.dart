5be61ae5f1cacbf9c6e0e84cf38d6284bcfa0807
Jochum van der Ploeg <jochum@vdploeg.net>
2022-02-21 00:04:17 +0100

    fix: Fixed and improved memory tests

---
+++ packages/deposit/test/order_by_test.dart
@@ -0,0 +1,10 @@
+import 'package:deposit/deposit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('OrderBy', () {
+    test('should be instantiated', () {
+      expect(OrderBy('key'), isNotNull);
+    });
+  });
+}
